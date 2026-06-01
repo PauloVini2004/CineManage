a3cade835734deb74d8e241a75d14b926448c9c3
Maksimus <gabriel.maksimus@ufrpe.br>
2026-06-01 10:37:54 -0300

    Organizando novamente

--- cinemanagerfx/src/main/java/module-info.java
+++ cinemanagerfx/src/main/java/module-info.java
@@ -1,15 +1,7 @@
 module br.ufrpe.cine_rural {
     requires javafx.controls;
     requires javafx.fxml;
-
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-
+    
     opens br.ufrpe.cine_rural.gui.controllers_telas to javafx.fxml;
     exports br.ufrpe.cine_rural.gui;
 }
