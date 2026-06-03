dc3ce409d31a7612d99e59802d49b8e8577b3836
JUS0110 <juliaellen20203ju@gmail.com>
2026-06-03 17:43:36 -0300

    Adicionei 1 produto no controller para implementar as funcionalidades na tela depois.

--- cinemanagerfx/src/main/java/br/ufrpe/cine_rural/gui/controllers_telas/ProdutoController.java
+++ cinemanagerfx/src/main/java/br/ufrpe/cine_rural/gui/controllers_telas/ProdutoController.java
@@ -1,5 +1,33 @@
 package br.ufrpe.cine_rural.gui.controllers_telas;
 
-public class ProdutoController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+public class ProdutoController {
+    @FXML
+    private Label nomeProduto1;
+
+    @FXML
+    private Label precoProduto1;
+
+    @FXML
+    private ImageView imagemProduto1;
+
+    @FXML
+    public void initialize() {
+
+        nomeProduto1.setText("Pipoca");
+
+        precoProduto1.setText("R$ 14,90");
+
+        imagemProduto1.setImage(
+                new Image(
+                        getClass().getResourceAsStream(
+                                "/br/ufrpe/cine_rural/gui/Imagens/Pipoca.jpg"
+                        )
+                )
+        );
+    }
 }
