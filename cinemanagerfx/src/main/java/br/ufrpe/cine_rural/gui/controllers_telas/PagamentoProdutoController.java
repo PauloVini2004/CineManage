85332362dd257e61f8979ff754108f1bba87de09
JUS0110 <juliaellen20203ju@gmail.com>
2026-06-13 22:12:40 -0300

    Ajustes para a implementação dos e-mails.

--- cinemanagerfx/src/main/java/br/ufrpe/cine_rural/gui/controllers_telas/PagamentoProdutoController.java
+++ cinemanagerfx/src/main/java/br/ufrpe/cine_rural/gui/controllers_telas/PagamentoProdutoController.java
@@ -197,6 +197,8 @@ public class PagamentoProdutoController {
                             email
                     );
 
+
+
             if (comboPagamento.getValue() == null) {
 
                 mostrarErro(
